65a2ba6685a2519bf95816356eae2456d4c2e78f
Kevin Martinez <ksmartinez2307@gmail.com>
2022-02-20 11:07:33 -0500

    Agregar id a la informacion del token

--- auth-jwt-service/src/main/java/com/zhopy/authjwtservice/dto/UserDTO.java
+++ auth-jwt-service/src/main/java/com/zhopy/authjwtservice/dto/UserDTO.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 
 @Data
 public class UserDTO implements Serializable {
+    private String userId;
     private String userName;
     private String email;
     private String password;

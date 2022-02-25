b67ee4c74bd2f09404ce252240742cb1da23795d
Kevin Martinez <ksmartinez2307@gmail.com>
2022-02-24 20:44:14 -0500

    Implementacion FeignClient para comunicacion entre servicios y config-service para manejar la configuracion de los servicios en un solo lugar

---
+++ user-service/src/main/java/com/zhopy/userservice/feignclients/RoleFeignClient.java
@@ -0,0 +1,16 @@
+package com.zhopy.userservice.feignclients;
+
+import com.zhopy.userservice.model.Role;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "role-service", url = "http://localhost:8001")
+public interface RoleFeignClient {
+
+    @GetMapping(value = "/api/role/detail/{roleCode}")
+    Role findByRoleCode(@PathVariable("roleCode") Long roleCode);
+
+    @GetMapping("/api/role/validate/{roleCode}")
+    boolean existsByRoleCode(@PathVariable("roleCode") Long roleCode);
+}

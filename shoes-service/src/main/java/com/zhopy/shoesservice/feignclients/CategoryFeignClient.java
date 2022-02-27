bf3af722d2ed69bb77c0a4f3cb1c98df14b3221c
Kevin Martinez <ksmartinez2307@gmail.com>
2022-02-26 21:09:50 -0500

    Correccion configuracion eureka

--- shoes-service/src/main/java/com/zhopy/shoesservice/feignclients/CategoryFeignClient.java
+++ shoes-service/src/main/java/com/zhopy/shoesservice/feignclients/CategoryFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "category-service", url = "http://localhost:8005")
+@FeignClient(name = "category-service")
 public interface CategoryFeignClient {
 
     @GetMapping(value = "/api/category/detail/{categoryCode}")
--- user-service/src/main/java/com/zhopy/userservice/feignclients/RoleFeignClient.java
+++ user-service/src/main/java/com/zhopy/userservice/feignclients/RoleFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "role-service", url = "http://localhost:8001")
+@FeignClient(name = "role-service")
 public interface RoleFeignClient {
 
     @GetMapping(value = "/api/role/detail/{roleCode}")
